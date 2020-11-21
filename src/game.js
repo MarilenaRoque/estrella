2bf8ae382d9861249967b4c33b7a1f59a62cf0f1
MarilenaRoque <marilenas@id.uff.br>
2020-11-20 21:49:32 -0300

    Set ball moves
    - Make the ball collide with the platform
    - Make the ball be lanced with the same force
    - But the part of the force is lost, because platform still moves

--- src/game.js
+++ src/game.js
@@ -6,7 +6,8 @@ class Game extends Phaser.Scene {
     preload() {
         this.load.image('background', 'images/background.jpg');
         this.load.image('gameOver', 'images/game-over.png');
-        this.load.image('platform', 'images/platform.png')
+        this.load.image('platform', 'images/platform.png');
+        this.load.image('ball', 'images/ball.png');
     }
 
     create() {
@@ -18,13 +19,29 @@ class Game extends Phaser.Scene {
         this.gameOverImage = this.add.image(400, 250, 'gameOver');
         this.gameOverImage.visible = false;
 
+
         //adding platform
         this.platformImage = this.physics.add.image(400, 470, 'platform');
         this.platformImage.body.allowGravity = false;
         this.cursors = this.input.keyboard.createCursorKeys();
+
+    
+        //adding ball
+        this.ballImage = this.physics.add.image(400, 30, 'ball');
+
+        //this piece of code make the platform collide with the ball,
+        // but the force of the ball make the platform move
+        this.physics.add.collider(this.platformImage, this.ballImage);
+
+        // this piece of code makes the ball be lanced with the same force that it touched
+        //the platform, but part of the force is lost because the platform moves
+        this.ballImage.setBounce(1);
+
     }
 
     update() {
+
+        //setting moves for the platform
         if (this.cursors.left.isDown) {
             this.platformImage.setVelocityX(-500);
         } else if (this.cursors.right.isDown) {
